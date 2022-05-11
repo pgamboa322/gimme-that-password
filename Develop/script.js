e4b35176e2c4b5c8139efa31fa9cc7799268faac
pgamboa322 <pgamboa322@gmail.com>
2022-05-10 21:37:17 -0500

    created lowercase, uppercase, number and special arrays

--- Develop/script.js
+++ Develop/script.js
@@ -10,6 +10,11 @@ function writePassword() {
   passwordText.value = password;
 }
 
+let lowercaseArr = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+let uppercaseArr = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+let numberArr = ["1","2","3","4","5","6","7","8","9","0"]
+let specialArr = ["!","@","#","$","%","&","*"]
+
 function generatePassword() {
   let passwordString = "";
   let passwordArray = [];
@@ -19,7 +24,26 @@ function generatePassword() {
   ); 
   if (passwordLength >128 || passwordLength <8) {
     return alert("Enter the correct character amount. Try again.")
+  }
 
+  let lowercase = confirm("Do you want lowercase characters?")
+  if (lowercase === true) {
+    passwordArray = passwordArray.concat(lowercaseArr);
+  }
+
+  let uppercase = confirm("Do you want uppercase characters?")
+  if (uppercase === true) {
+    passwordArray = passwordArray.concat(uppercaseArr);
+  }
+
+  let number = confirm("Do you want number characters?")
+  if (number === true) {
+    passwordArray = passwordArray.concat(numberArr);
+  }
+
+  let special = confirm("Do you want special characters?")
+  if (special === true) {
+    passwordArray = passwordArray.concat(specialArr);
   }
 }
 
