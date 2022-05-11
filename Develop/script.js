310c845424a21ddd87949bf6a64e1196364c760a
pgamboa322 <pgamboa322@gmail.com>
2022-05-10 20:54:07 -0500

    created logic for password length requirement

--- Develop/script.js
+++ Develop/script.js
@@ -1,16 +1,26 @@
 // Assignment code here
 
-
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
   passwordText.value = password;
+}
 
+function generatePassword() {
+  let passwordString = "";
+  let passwordArray = [];
+
+  let passwordLength = parseInt (
+    prompt("Choose a length of at least 8 characters and no more than 128 characters")
+  ); 
+  if (passwordLength >128 || passwordLength <8) {
+    return alert("Enter the correct character amount. Try again.")
+
+  }
 }
 
 // Add event listener to generate button
