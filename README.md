14f6a548691c0bf40b255e624e4081101eba1892
pgamboa322 <pgamboa322@gmail.com>
2022-05-10 23:45:01 -0500

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # gimme-that-password
+
+## Installation
+An application that will run in the browser and feature dynamically updated. Application features polished UI and is responsive, ensuring adaptation to multiple screen sizes.
+
+To build this webpage I used: - HTML - Javascript 
+
+## About this Webpage
+Undecided is a webpage designed to generate a random password that includes lowercase, uppercase, numbers and special characters. 
+
+## User Story
+GIVEN I need a new, secure password
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+WHEN asked for character types to include in the password
+THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
+
+## Contributors
+Paulina Gamboa
+
+## Screenshots
+
+![This image shows the password generator.](./assets/images/screenshot1.png)
+![This image shows the prompt for character length.](./assets/images/screenshot2.png)
+![This image shows the lowercase confirm.](./assets/images/screenshot3.png)
+![This image shows the uppercase confirm.](./assets/images/screenshot4.png)
+![This image shows the number confirm.](./assets/images/screenshot5.png)
+![This image shows the special character confirm.](./assets/images/screenshot6.png)
+![This image shows the generated password.](./assets/images/screenshot7.png)
+
+
+## Links
+https://pgamboa322.github.io/gimme-that-password/
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,26 @@
 // Assignment code here
 
 // Get references to the #generate element
-let generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  let password = generatePassword();
-  let passwordText = document.querySelector("#password");
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
 
 // create arrays for different defined variables
-let lowercaseArr = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-let uppercaseArr = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-let numberArr = ["1","2","3","4","5","6","7","8","9","0"]
-let specialArr = ["!","@","#","$","%","&","*"]
+var lowercaseArr = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var uppercaseArr = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var numberArr = ["1","2","3","4","5","6","7","8","9","0"]
+var specialArr = ["!","@","#","$","%","&","*"]
 
 function generatePassword() {
-  let passwordString = "";
-  let passwordArray = [];
+  var passwordString = "";
+  var passwordArray = [];
 //parse takes a string value and makes integer
-  let passwordLength = parseInt (
+  var passwordLength = parseInt (
     prompt("Choose a length of at least 8 characters and no more than 128 characters")
   ); 
   //if not between 8-128 characters 
@@ -29,29 +29,29 @@ function generatePassword() {
     return generatePassword();
   }
 // adding lowercase characters to password array
-  let lowercase = confirm("Do you want lowercase characters?")
+  var lowercase = confirm("Do you want lowercase characters?")
   if (lowercase === true) {
     passwordArray = passwordArray.concat(lowercaseArr);
   }
 // adding uppercase characters to password array
-  let uppercase = confirm("Do you want uppercase characters?")
+  var uppercase = confirm("Do you want uppercase characters?")
   if (uppercase === true) {
     passwordArray = passwordArray.concat(uppercaseArr);
   }
 // adding number characters to password array
-  let number = confirm("Do you want number characters?")
+  var number = confirm("Do you want number characters?")
   if (number === true) {
     passwordArray = passwordArray.concat(numberArr);
   }
 // adding special characters to password array
-  let special = confirm("Do you want special characters?")
+  var special = confirm("Do you want special characters?")
   if (special === true) {
     passwordArray = passwordArray.concat(specialArr);
   }
 //loop through password array number of password length (8-128)
 //create string of password array characters
 //floor rounds down, passwordarray.length = 26+26+...
-  for (let i = 0; i < passwordLength; i++) {
+  for (var i = 0; i < passwordLength; i++) {
     passwordString += passwordArray[Math.floor(Math.random() * passwordArray.length)];
   }
   return passwordString;
